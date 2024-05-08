dd6e4cb5054c57143e6482eed5cc8c1a1b0ad563
MyCodeNotWorking <126179690+MyCodeNotWorking@users.noreply.github.com>
2024-05-08 16:31:33 +0200

    hab den spieler mit bewegung hinzugefügt
    saved

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -12,6 +12,9 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed*delta
 	
-	var p_width = $CollisionShape2D.size.x
-	var p_height = $CollisionShape2D.size.y
-	position.x = clamp(position.x, p_width, get_viewport_rect().size.x)
+	var w = $CollisionShape2D.shape.size.x / 2
+	var h = $CollisionShape2D.shape.size.y / 2
+	var vw = get_viewport_rect().size.x - w
+	var vh = get_viewport_rect().size.y - h
+	position.x = clamp(position.x, w, vw)
+	position.y = clamp(position.y, h, vh)
